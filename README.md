e8492cf2301cb8f73ef03fb3482a66ecbb56d8b3
Adarsh Rao <adarshrao@users.noreply.github.com>
2018-05-30 20:36:03 +0530

    Updating the license info and readme

--- README.md
+++ README.md
@@ -4,15 +4,11 @@ Expand a shortened URL to the full URL.
 
 This function expands a shortened URL (like in Twitter tweets) to the full URL.
 
-It is inspired on code from urlclean.
-
-A lot of the same functionality can be achieved by using urllib2.url but this fetches the full page with the Python browser identification. Unfortunately some sites block your access if you use Python as the browser identification. This is why I wrote my own function.
+A derivative of the original by Cudeso - https://github.com/cudeso/expandurl . I used the 2to3.py to convert the code into Python3.
 
 This function reads the HTTP redirect codes and follows them. It should work for both http and https.
 
 The given user agent is the one used by the Google bot.
-
-Some redirects do not contain the "http://mysite.com" in the Location header. This has been dealt with.
 
 The function reads the URLs from a file (one URL per line) and fetches the redirects.
 
@@ -22,31 +18,13 @@ If an error occurs then the original URL is returned.
 
 Usage
 ------------
-```expandurl.py urllist```
+```expandurl.py urlfile.txt```
 
 Output
 ------------
 ```
-http://t.co/t2XiWoXL9m
- 
- \__http://m.motherjones.com/politics/2015/01/republicans-free-city-wifi-municipal-broadband-socialism?utm_content=bufferc0538&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer
-http://t.co/WQpQwmPFGd
- 
- \__http://deredactie.be/cm/vrtnieuws/politiek/1.2253446
-https://t.co/w1Sd1dmUL3
- 
- \__https://nomadphp.com/2014/12/19/nomadphp-2015-03-eu/
-http://t.co/BGZ52avOB5
- 
- \__http://blog.dynamoo.com/2015/02/an-analysis-of-reported-equation-group.html?spref=tw
-https://t.co/K04nbfyWST
- 
- \__https://bitbucket.org/mihaila/bindead/wiki/Home
-```
+b'https://t.co/kNgW6nQor6\n'
+ \__https://haptic.al/
 
-urlclean
-------------
-module that resolves and cleans up urls 
- https://github.com/stef/urlclean
-
-
+b'https://t.co/e9UeNQPxk6'
+ \__https://mixpanel.com
--- expandurl.py
+++ expandurl.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 
 #
-# Expand shortened URLs
-#
 # Expand shortened URLs based on the return HTTP redirect headers
-# Inspired by 'urlclean' by Stefan Marsiske
-#   https://github.com/stef/urlclean
-# 
-#  Koen Van Impe on 2015-02-27
+#
+# This is the Python3 version of the original Python2 Script by Cudeso at
+# https://github.com/cudeso/expandurl
+#
+#   Adarsh Rao on 2018-05-30
+#   adarshnet at gmail dot com
+#   license : FreeBSD 2 Clause (Same as prev.)
+#
+#   Original License#
+#   Koen Van Impe on 2015-02-27
 #   koen dot vanimpe at cudeso dot be
 #   license New BSD : http://www.vanimpe.eu/license
 #
 
 import sys
-import urllib, httplib
+import urllib.request, urllib.parse, urllib.error, http.client
 
 url_ua = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
 url_timeout = 15
-
 
 def unshortenURL(url):
     currenturl = url.strip()
@@ -25,43 +28,43 @@ def unshortenURL(url):
     while currenturl != previousurl:
 
         try:
-            httprequest = httplib.urlsplit(currenturl)
+            httprequest = http.client.urlsplit(currenturl)
             scheme = httprequest.scheme.lower()
             netloc = httprequest.netloc.lower()
             previousurl = currenturl
             if scheme == 'http':
-                conn = httplib.HTTPConnection(netloc, timeout=url_timeout)
+                conn = http.client.HTTPConnection(netloc, timeout=url_timeout)
                 req = currenturl[7+len(netloc):]
                 location = "%s://%s" % (scheme, netloc)
             elif scheme=='https':
-                conn = httplib.HTTPSConnection(netloc, timeout=url_timeout)
+                conn = http.client.HTTPSConnection(netloc, timeout=url_timeout)
                 req = currenturl[8+len(netloc):]
-                location = "%s://%s" % (scheme, netloc)           
+                location = "%s://%s" % (scheme, netloc)
 
             conn.request("HEAD", req, None, {'User-Agent': url_ua,'Accept': '*/*',})
             res = conn.getresponse()
 
             if res.status in [301, 304]:
                 currenturl = res.getheader('Location')
-                httprequest_redirect = httplib.urlsplit(currenturl)
+                httprequest_redirect = http.client.urlsplit(currenturl)
 
                 if httprequest_redirect.scheme.lower() != 'http' and httprequest_redirect.scheme.lower() != 'https':
-                    # currenturl does not contain http(s) 
+                    # currenturl does not contain http(s)
                     currenturl = "%s://%s%s" % (scheme, netloc,currenturl)
         except:
             currenturl = url
 
     return currenturl
-    
+
 def main():
     if len(sys.argv) == 2:
-        f = open (sys.argv[1])
-        for url in f:
-            unshorten = unshortenURL(url)
-            print "%s \n \__%s" % (url.encode('utf-8'), unshorten)
+        with open(sys.argv[1]) as f:
+            for url in f:
+                unshorten = unshortenURL(url)
+                print("%s \n \__%s" % (url.encode('utf-8'), unshorten))
 
     else:
-        print "Please provide a file containing the URLs (one per line)"
+        print("Please provide a file containing the URLs (one per line)")
 
 if __name__ == '__main__':
     main()
